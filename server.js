df58cefd06be7e86ae37512cbde53d729b9a3550
juaning <juaning@gmail.com>
2017-12-04 11:41:49 -0300

    Update .gitignore with config files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 //first we import our dependencies...
 var express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 //and create our instances
@@ -12,6 +12,10 @@ var router = express.Router();
 //set our port to either a predetermined port number
 //if you have set it up or 3001
 var port = process.env.API_PORT || 3001;
+
+//db config
+mongoose.connect('mongodb://<dbuser>:<dbpassword>'
+    + '@ds129796.mlab.com:29796/champs-coq');
 
 //now we should configure the API to use bodyParser and look for
 //JSON data in the request body
